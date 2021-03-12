b6a186f154e9df420313b90641191e858dfa3acc
stefan122 <stefan.jeftic122@gmail.com>
2021-03-12 13:17:17 +0100

    Update

---
+++ .config/awesome/lain/widget/bright.lua
@@ -0,0 +1,44 @@
+--[[
+
+     Licensed under GNU General Public License v2
+      * (c) 2013, Luca CPZ
+      * (c) 2010, Adrian C. <anrxc@sysphere.org>
+
+--]]
+
+local helpers = require("lain.helpers")
+local awful = require("awful")
+local shell   = require("awful.util").shell
+local wibox   = require("wibox")
+local string  = string
+
+-- ALSA volume
+-- lain.widget.alsa
+
+local function factory(args)
+    local bright     = { widget = wibox.widget.textbox() }
+    local args     = args or {}
+    local timeout  = args.timeout or 5
+    local settings = args.settings or function() end
+    local format = { shell, "-c", "xbacklight -get | xargs printf '%.0f'" }
+
+    function bright.update()
+    
+    helpers.async(format, function(brightness)
+        bright_now = brightness
+        widget = bright.widget
+        settings()
+    end)
+--    bright_now = io.popen("xbacklight -get | xargs printf '%.0f'"):read("*l")
+
+ --   widget = bright.widget
+ --   settings()
+
+    end
+
+    helpers.newtimer("bright_now", timeout, bright.update)
+
+    return bright
+end
+
+return factory
