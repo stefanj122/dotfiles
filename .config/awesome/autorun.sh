a722245b43cbaa870732b2740e562781a49bd7b0
Stefanj122 <stefan.jeftic122@gmail.com>
2021-08-31 00:04:26 +0200

    Synaptics driver

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -11,5 +11,6 @@ numlockx on &
 run "sus.sh"
 xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Synaptics Tap Action' 0, 0, 0, 0, 1, 3, 2 &
 xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Synaptics Two-Finger Scrolling' 1, 1 &
-xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Synaptics Scrolling Distance' -106, 108 &
+xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Synaptics Scrolling Distance' -86, -88 &
+xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Synaptics Off' 2 &
 #run "nm-applet &"
