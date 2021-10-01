21f2b7a85bcf7a394fe1b251dc13fd1d64de7162
Stefanj122 <stefan.jeftic122@gmail.com>
2021-10-01 16:45:57 +0200

    Adding light-locker

--- sus.sh
+++ sus.sh
@@ -3,6 +3,8 @@
 # Only exported variables can be used within the timer's command.
 export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
 
+(light-locker) &
+
 # Run xidlehook
 xidlehook \
   `# Don't lock when there's audio playing` \
