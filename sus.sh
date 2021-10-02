9a9f472a4df20a2d095772896c138f1d24a5e64f
Stefanj122 <stefan.jeftic122@gmail.com>
2021-10-02 12:11:30 +0200

    Fix sus.sh

--- sus.sh
+++ sus.sh
@@ -3,21 +3,28 @@
 # Only exported variables can be used within the timer's command.
 export PRIMARY_DISPLAY="$(xrandr | awk '/ primary/{print $1}')"
 
-(light-locker) &
+if  pgrep -f xidlehook > 0 && pgrep -f light-locker > 0 
+then
+	echo "Xidlehook and light-locker is working."
+else
 
-# Run xidlehook
-xidlehook \
-  `# Don't lock when there's audio playing` \
-  --not-when-audio \
-  `# Dim the screen after 60 seconds, undim if user becomes active` \
-  --timer 300 \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
-  `# Undim & lock after 10 more seconds` \
-  --timer 900 \
-    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1; light-locker-command -l' \
-    '' \
-  `# Finally, suspend an hour after it locks` \
-  --timer 3600 \
-    'systemctl suspend' \
-    ''
+	(light-locker) &
+
+	# Run xidlehook
+	xidlehook \
+	  `# Don't lock when there's audio playing` \
+	  --not-when-audio \
+	  `# Dim the screen after 60 seconds, undim if user becomes active` \
+	  --timer 300 \
+	    'xrandr --output "$PRIMARY_DISPLAY" --brightness .1' \
+	    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1' \
+	  `# Undim & lock after 10 more seconds` \
+	  --timer 900 \
+	    'xrandr --output "$PRIMARY_DISPLAY" --brightness 1; light-locker-command -l' \
+	    '' \
+	  `# Finally, suspend an hour after it locks` \
+	  --timer 3600 \
+	    'systemctl suspend' \
+	    ''
+fi
+
