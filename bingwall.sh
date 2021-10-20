7fafef0682f8e4c61ef477cb8611e68dc47a88eb
Stefanj122 <stefan.jeftic122@gmail.com>
2021-10-20 11:39:12 +0200

    FIx big

--- bingwall.sh
+++ bingwall.sh
@@ -5,8 +5,8 @@ curl "https://www.bing.com/HPImageArchive.aspx?format=rss&idx=0&n=1&mkt=en-US" \
 | xmllint --xpath "/rss/channel/item/link/text()" - \
 | sed 's/1366x768/1920x1080/g' \
 )
-curl "https://www.bing.com$urlpath" \
-| feh --bg-fill -
+curl "https://www.bing.com$urlpath"  --output /home/stefanj/Pictures/bingwall.png \
+| feh --bg-fill /home/stefanj/Pictures/bingwall.png
 
 PID=$(pgrep -fc bingwall)
 
