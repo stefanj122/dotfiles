88515245cbe239af83a27d949912bc0d685899ae
Stefanj122 <stefan.jeftic122@gmail.com>
2021-10-29 15:54:01 +0200

    Fix suspending toggle

--- kill.sh
+++ kill.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-
-(killall light-locker) &
-(killall xidlehook) &
-notify-send 'Suspending is disabled!'
-
+if  pgrep -f xidlehook > 0 && pgrep -f light-locker > 0
+then
+    (killall light-locker) &
+    (killall xidlehook) &
+    notify-send 'Suspending is disabled!'
+else
+    notify-send 'Suspending is enabled!'
+    exec /usr/bin/sus.sh
+fi
 
 
