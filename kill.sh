7a0ab5617015a0bbe71a8fc1c933ef9d6dbd849b
Stefanj122 <stefan.jeftic122@gmail.com>
2021-11-20 19:19:52 +0100

    Change

--- kill.sh
+++ kill.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if  pgrep -f xidlehook > 0 && pgrep -f light-locker > 0
+if  pgrep -f xidlehook > 0 #&& pgrep -f light-locker > 0
 then
-    (killall light-locker) &
+#    (killall light-locker) &
     (killall xidlehook) &
     notify-send 'Suspending is disabled!'
 else
