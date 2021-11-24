8f861390033042be15272cd7742fbad360c2c966
Stefanj122 <stefan.jeftic122@gmail.com>
2021-11-24 17:52:59 +0100

    fandb

--- dependence.sh
+++ dependence.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-yay -S xidlehook nerd-fonts-mononoki exa ttf-mononoki xf86-input-synaptics brave-bin fish starfish notification-daemon optimus-manager-qt xkb-switch apulse pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-qt pulseaudio-zeroconf pulsemixer pavucontrol picom lxapperance htop mailspring mpv neofetch pacmanfm qt5ct ranger smplayer rofi rtorrent libpulse dmenu-distrotube-git st-distrotube-git numlockx udiskie
+yay -S xidlehook nerd-fonts-mononoki exa ttf-mononoki xf86-input-synaptics brave-bin fish starfish notification-daemon optimus-manager-qt xkb-switch apulse pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-qt pulseaudio-zeroconf pulsemixer pavucontrol picom lxapperance htop mailspring mpv neofetch pacmanfm qt5ct ranger smplayer rofi rtorrent libpulse dmenu-distrotube-git st-distrotube-git numlockx udiskie udisks2
 
