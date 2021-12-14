e88c09701de43051fe22154a5d936ad1f2bef2cf
Stefanj122 <stefan.jeftic122@gmail.com>
2021-12-14 15:50:57 +0100

    Change

--- switch.sh
+++ switch.sh
@@ -3,16 +3,52 @@
 pom1="Nvidia
 Hybrid
 Integrated"
-prompt="Switch to:"
+prompt="Choose:"
+pom2="Switch graphic
+Power menu"
+pom="$(optimus-manager --print-mode | cut -d ':' -f 2)"
 
-#cmd=$("$file" | dmenu "$@" )
-cmd=$( echo "$pom1" | dmenu -p "$prompt")
-    if [ $cmd == "Nvidia" ]; then
-        optimus-manager --switch nvidia --no-confirm
-    else if [ $cmd == "Hybrid" ]; then
-        optimus-manager --switch hybrid --no-confirm
-    else if [ $cmd == "Integrated" ]; then
-        optimus-manager --switch integrated --no-confirm
+cmd=$( echo "$pom2" | dmenu -p "$prompt")
+
+
+if [ "$cmd" = "Switch graphic" ]; then
+
+    if [ "$pom" == "nvidia" ]; then
+        pom1="Hybrid
+Integrated"
+    elif [ "$pom" == "hybrid" ]; then
+        pom1="Nvidia
+Integrated"
+    else
+        pom1="Nvidai
+Hybrid"
     fi
+#cmd=$("$file" | dmenu "$@" )
+    cmd=$( echo "$pom1" | dmenu -p "$prompt")
+        if [ "$cmd" == "Nvidia" ]; then
+            optimus-manager --switch nvidia --no-confirm &
+        elif [ "$cmd" == "Hybrid" ]; then
+            optimus-manager --switch hybrid --no-confirm &
+        elif [ "$cmd" == "Integrated" ]; then
+            optimus-manager --switch integrated --no-confirm &
+        else 
+            echo "Don't work!!!"
+        fi
+elif [ "$cmd" == "Power menu" ]; then
+    pom1="Reboot
+Logout
+Sleep
+Shutdown"
+    cmd=$( echo "$pom1" | dmenu -p "$prompt")
+    if [ "$cmd" == "Reboot" ]; then
+        systemctl reboot
+    elif [ "$cmd" == "Sleep" ]; then
+        systemctl suspend
+    elif [ "$cmd" == "Shutdown" ]; then
+        systemctl poweroff
+    elif [ "$cmd" == "Logout" ]; then
+        pkill $DESKTOP_SESSION
+    fi
+fi
 
-exit 0
+
