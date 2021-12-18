f60edf235b0341176a97aaa1d6edda478131862a
Stefanj122 <stefan.jeftic122@gmail.com>
2021-12-18 16:56:45 +0100

    Change

--- bright.sh
+++ bright.sh
@@ -9,7 +9,7 @@ msgTag="mybrightness"
 bright="$(xbacklight -get | xargs printf '%.0f')"
     # Show the volume notification
     dunstify -a "change" -i brightness -h string:x-dunst-stack-tag:$msgTag \
-    -h int:value:"$bright" "Brightness: ${bright}%" -r 16 -t 2000 -I /home/stefanj/.config/awesome/themes/powerarrow-blue/icons/bright.png
+    -h int:value:"$bright" "Brightness: ${bright}%" -r 15 -t 2000 -I /home/stefanj/.config/awesome/themes/powerarrow-blue/icons/bright.png
 
 # Play the volume changed sound
 #canberra-gtk-play -i audio-volume-change -d "changeVolume"
