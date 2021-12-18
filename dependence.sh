798dfcdca748a0f0722e1c24adeeab512323eb71
Stefanj122 <stefan.jeftic122@gmail.com>
2021-12-18 17:02:00 +0100

    Change

--- dependence.sh
+++ dependence.sh
@@ -2,3 +2,12 @@
 
 paru -S xidlehook nerd-fonts-mononoki exa ttf-mononoki xf86-input-synaptics brave-bin fish starfish notification-daemon optimus-manager-qt xkb-switch apulse pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-qt pulseaudio-zeroconf pulsemixer pavucontrol picom lxapperance htop mailspring mpv neofetch pacmanfm qt5ct ranger smplayer rofi libpulse dmenu-distrotube-git st-distrotube-git numlockx udiskie udisks2 awesome git neovim mlocate sed playerctl xorg-xbacklight dunst youtube-dl arc-gtk-theme blueman bluez bluez-utils bluez-qt bluez-libs pcmanfm feh kdeconnect lm_sensors xtrlock steam steam-fonts transmission-gtk unrar unzip brightnessctl
 
+mkdir ~/.scripts
+sudo cp bingwall.sh /usr/bin
+sudo cp bright.sh /usr/bin
+sudo cp hdmi.sh /usr/bin
+sudo cp sus.sh /usr/bin
+sudo cp toggle.sh /usr/bin
+sudo cp vol.sh /usr/bin
+cp kill.sh ~/.scripts
+cp switch.sh ~/.scripts
