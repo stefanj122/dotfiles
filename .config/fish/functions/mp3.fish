92ddd75c39b77380ff67b072947eaad8629ed82e
Stefanj122 <stefan.jeftic122@gmail.com>
2022-01-30 13:59:10 +0100

    Change

--- .config/fish/functions/mp3.fish
+++ .config/fish/functions/mp3.fish
@@ -1,3 +1,3 @@
-function mp3 --description 'alias mp3=youtube-dl -x --audio-format mp3'
-  youtube-dl -x --audio-format mp3 $argv; 
+function mp3 --wraps='youtube-dl --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s"' --description 'alias mp3=youtube-dl --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s"'
+  youtube-dl --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" $argv; 
 end
--- dependence.sh
+++ dependence.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-paru -S xidlehook nerd-fonts-mononoki exa ttf-mononoki xf86-input-synaptics brave-bin fish starfish notification-daemon optimus-manager-qt xkb-switch apulse pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-qt pulseaudio-zeroconf pulsemixer pavucontrol picom lxapperance htop mailspring mpv neofetch pacmanfm qt5ct ranger smplayer rofi libpulse dmenu-distrotube-git st-distrotube-git numlockx udiskie udisks2 awesome git neovim mlocate sed playerctl dunst youtube-dl arc-gtk-theme blueman bluez bluez-utils bluez-qt bluez-libs pcmanfm feh kdeconnect lm_sensors xtrlock steam steam-fonts transmission-gtk unrar unzip brightnessctl
+paru -S xidlehook nerd-fonts-mononoki exa ttf-mononoki xf86-input-synaptics brave-bin fish starfish notification-daemon optimus-manager-qt xkb-switch apulse pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-qt pulseaudio-zeroconf pulsemixer pavucontrol picom lxapperance htop mailspring mpv neofetch pacmanfm qt5ct ranger smplayer rofi libpulse dmenu-distrotube-git st-distrotube-git numlockx udiskie udisks2 awesome git neovim mlocate sed playerctl dunst youtube-dl arc-gtk-theme blueman bluez bluez-utils bluez-qt bluez-libs pcmanfm feh kdeconnect lm_sensors xtrlock steam steam-fonts transmission-gtk unrar unzip brightnessctl papirus-icon-theme
 
 mkdir ~/.scripts
 sudo cp bingwall.sh /usr/bin
