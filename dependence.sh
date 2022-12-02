8d2e593b4d875c9d5f136af7bde43e538f0e133e
stefanj122 <stefan.jeftic122@gmail.com>
2022-12-02 15:18:10 +0100

    new changes

--- dependence.sh
+++ dependence.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-pacman -S ttf-droid exa xf86-input-synaptics fish pulseaudio  picom htop  imwheel 
-paru -S xidlehook nerd-fonts-mononoki ttf-mononoki brave-bin starship notification-daemon optimus-manager-qt xkb-switch apulse pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-qt pulseaudio-zeroconf pulsemixer pavucontrol lxapperance mailspring mpv neofetch pacmanfm qt5ct ranger smplayer rofi libpulse dmenu-distrotube-git st-distrotube-git numlockx udiskie udisks2 awesome git neovim mlocate playerctl dunst youtube-dl arc-gtk-theme blueman bluez bluez-utils bluez-qt bluez-libs pcmanfm feh kdeconnect lm_sensors xtrlock steam steam-fonts transmission-gtk unrar unzip brightnessctl papirus-icon-theme maim libreoffice-fresh bc alsa-utils alsa-plugins 
+pacman -S ttf-droid exa xf86-input-synaptics fish pulseaudio  picom htop  imwheel unrar unzip youtube-dl wireplumer neovim vim udisks2 udiskie ttf-jotpixels ttf-hack starship smplayer rofi qutebrowser pulsemixer pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse pipewire-zeroconf pcmanfm pavucontrol papirus-icon-theme pamixer okular numlockx ntfs-3g neofetch feh dunst alsa-utils alsa-plugins arc-gtk-theme 
+paru -S xidlehook nerd-fonts-mononoki ttf-mononoki brave-bin notification-daemon optimus-manager-qt xkb-switch apulse pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-qt pulseaudio-zeroconf lxapperance mailspring mpv pacmanfm qt5ct ranger libpulse awesome git mlocate playerctl blueman bluez bluez-utils bluez-qt bluez-libs kdeconnect lm_sensors xtrlock steam steam-fonts transmission-gtk brightnessctl maim libreoffice-fresh bc 
 
 mkdir ~/.scripts
 sudo cp bingwall.sh /usr/bin
