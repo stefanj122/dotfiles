35b4d7c6a0732ade015ba5fddf8a9f3280b1d388
stefanj122 <stefan.jeftic122@gmail.com>
2022-12-20 20:39:34 +0100

    added emacs daemon

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -9,13 +9,16 @@ function run {
 
 numlockx on &
 run "sus.sh"
-xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Synaptics Tap Action' 0, 0, 0, 0, 1, 3, 2 &
-xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Synaptics Two-Finger Scrolling' 1, 1 &
-xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Synaptics Scrolling Distance' -86, -88 &
-syndaemon -i 0.5 -t -K -d &
+#xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Synaptics Tap Action' 0, 0, 0, 0, 1, 3, 2 &
+#xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Synaptics Two-Finger Scrolling' 1, 1 &
+#xinput set-prop 'SynPS/2 Synaptics TouchPad' 'Synaptics Scrolling Distance' -86, -88 &
+#syndaemon -i 0.5 -t -K -d &
 
+xinput set-prop 'SynPS/2 Synaptics TouchPad' 'libinput Tapping Enabled' 1 &
+xinput set-prop 'SynPS/2 Synaptics TouchPad' 'libinput Natural Scrolling Enabled' 1 &
 bingwall.sh &
 run "nm-applet "&
 run "optimus-manager-qt "&
 xset -dpms &
 xset s off &
+emacs --daemon -q &
