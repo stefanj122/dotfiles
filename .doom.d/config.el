f3ffaa83ab95f54702fd2158906d271af3a574c8
stefanj122 <stefan.jeftic122@gmail.com>
2022-12-20 17:44:01 +0100

    Added doom config

---
+++ .doom.d/config.el
@@ -0,0 +1,110 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; (set-face-attribute 'default nil
+;;   :font "Source Code Pro"
+;;   :height 130
+;;   :weight 'medium)
+;; (set-face-attribute 'variable-pitch nil
+;;   :font "Source Code Pro"
+;;   :height 140
+;;   :weight 'medium)
+;; (set-face-attribute 'fixed-pitch nil
+;;   :font "Source Code Pro"
+;;   :height 130
+;;   :weight 'medium)
+;; ;; Makes commented text and keywords italics.
+;; ;; This is working in emacsclient but not emacs.
+;; ;; Your font must have an italic face available.
+;; (set-face-attribute 'font-lock-comment-face nil
+;;   :slant 'italic)
+;; (set-face-attribute 'font-lock-keyword-face nil
+;;   :slant 'italic)
+
+;; ;; Uncomment the following line if line spacing needs adjusting.
+;; (setq-default line-spacing 0.12)
+
+;; ;; Needed if using emacsclient. Otherwise, your fonts will be smaller than expected.
+;; (add-to-list 'default-frame-alist '(font . "Source Code Pro-11"))
+;; ;; changes certain keywords to symbols, such as lamda!
+;; (setq global-prettify-symbols-mode t)
+(setq doom-font (font-spec :family "Source Code Pro" :size 18)
+      doom-big-font (font-spec :family "Source Code Pro" :size 36)
+      doom-variable-pitch-font (font-spec :family "Source Code Pro" :size 20)
+      doom-unicode-font (font-spec :family "Source Code Pro")
+      doom-serif-font (font-spec :family "Source Code Pro" :weight 'light))
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+(global-set-key (kbd "C-c v") #'clipboard-yank)
+(global-set-key (kbd "C-c c") #'clipboard-kill-ring-save)
+(global-set-key (kbd "C-c x") #'clipboard-kill-region)
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
