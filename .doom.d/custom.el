64973c4e5a808c2e70e9a66152e5111078ea7dee
stefanj122 <stefan.jeftic122@gmail.com>
2022-12-23 15:42:09 +0100

    Adding emacs conig

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,10 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(doom-dracula))
  '(custom-safe-themes
    '("944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" default))
  '(package-selected-packages
-   '(magit ac-js2 js-format lua-mode js2-refactor xref-js2 flow-js2-mode)))
+   '(company-lua flycheck-elsa flycheck-package company-phpactor py-snippets format-all php-boris php-refactor-mode company-php ac-php nodejs-repl yasnippet-snippets js-react-redux-yasnippets magit ac-js2 js-format lua-mode js2-refactor xref-js2 flow-js2-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,3 +15,4 @@
  ;; If there is more than one, they won't work right.
  )
 (put 'customize-face 'disabled nil)
+(put 'customize-themes 'disabled nil)
