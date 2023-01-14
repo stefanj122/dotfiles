8cf5b0058f805a6f01f659c56dd662e00f6276c9
stefanj122 <stefan.jeftic122@gmail.com>
2023-01-14 15:17:43 +0100

    Modification in Week9

---
+++ bingwall.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import fs from "fs";
+import path from "path";
+import { exec } from "child_process";
+
+exec("feh --bg-fill /home/stefanj/Pictures/bingwall.png");
+
+async function bingwall() {
+  const save = path.resolve("./..", "Pictures", "bingwall.jpg");
+  const url2 =
+    "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US";
+
+  try {
+    const imageUrl = await axios.get(url2);
+    const url = "https://bing.com" + imageUrl.data.images[0].url;
+
+    const response = await axios({
+      url,
+      method: "GET",
+      responseType: "stream",
+    });
+
+    response.data.pipe(fs.createWriteStream(save));
+    console.log("Image saved successfully");
+    exec("feh --bg-fill /home/stefanj/Pictures/bingwall.png");
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+exec("pgrep -fc 'node bingwall'", (err, stdout, stderr) => {
+  if (Number(stdout) <= 1) {
+    setInterval(() => bingwall(), 1000 * 60 * 60);
+  }
+});
