b485c8dd8e969c14c6f431d251076d6dc030e53a
stefanj122 <stefan.jeftic122@gmail.com>
2023-02-10 22:30:00 +0100

    switch htop to btop

--- dependence.sh
+++ dependence.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-pacman -S ttf-droid exa xf86-input-synaptics fish pulseaudio  picom htop  imwheel unrar unzip youtube-dl wireplumer neovim vim udisks2 udiskie ttf-jotpixels ttf-hack starship smplayer rofi qutebrowser pulsemixer pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse pipewire-zeroconf pcmanfm pavucontrol papirus-icon-theme pamixer okular numlockx ntfs-3g neofetch feh dunst alsa-utils alsa-plugins arc-gtk-theme 
+pacman -S ttf-droid exa xf86-input-synaptics fish pulseaudio  picom btop  imwheel unrar unzip youtube-dl wireplumer neovim vim udisks2 udiskie ttf-jotpixels ttf-hack starship smplayer rofi qutebrowser pulsemixer pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse pipewire-zeroconf pcmanfm pavucontrol papirus-icon-theme pamixer okular numlockx ntfs-3g neofetch feh dunst alsa-utils alsa-plugins arc-gtk-theme 
 paru -S xidlehook nerd-fonts-mononoki ttf-mononoki brave-bin notification-daemon optimus-manager-qt xkb-switch apulse pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-qt pulseaudio-zeroconf lxapperance mailspring mpv pacmanfm qt5ct ranger libpulse awesome git mlocate playerctl blueman bluez bluez-utils bluez-qt bluez-libs kdeconnect lm_sensors xtrlock steam steam-fonts transmission-gtk brightnessctl maim libreoffice-fresh bc 
 
 mkdir ~/.scripts
