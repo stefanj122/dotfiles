d9648721f3799a0d3dd7180c42a7a4719c370f66
stefanj122 <stefan.jeftic122@gmail.com>
2023-03-06 19:49:48 +0100

    Update dependence.sh

--- dependence.sh
+++ dependence.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-pacman -S ttf-droid exa xf86-input-synaptics fish pulseaudio  picom btop  imwheel unrar unzip youtube-dl wireplumer neovim vim udisks2 udiskie ttf-jotpixels ttf-hack starship smplayer rofi qutebrowser pulsemixer pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse pipewire-zeroconf pcmanfm pavucontrol papirus-icon-theme pamixer okular numlockx ntfs-3g neofetch feh dunst alsa-utils alsa-plugins arc-gtk-theme 
-paru -S xidlehook nerd-fonts-mononoki ttf-mononoki brave-bin notification-daemon optimus-manager-qt xkb-switch apulse pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-qt pulseaudio-zeroconf lxapperance mailspring mpv pacmanfm qt5ct ranger libpulse awesome git mlocate playerctl blueman bluez bluez-utils bluez-qt bluez-libs kdeconnect lm_sensors xtrlock steam steam-fonts transmission-gtk brightnessctl maim libreoffice-fresh bc 
+pacman -S ttf-droid ttf-mononoki-nerd exa xf86-input-synaptics fish  picom btop  imwheel unrar unzip youtube-dl wireplumer neovim vim udisks2 udiskie ttf-jotpixels ttf-hack starship smplayer rofi qutebrowser pulsemixer libpipewire pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse pipewire-zeroconf pcmanfm pavucontrol papirus-icon-theme pamixer okular numlockx ntfs-3g neofetch feh dunst alsa-utils alsa-plugins arc-gtk-theme 
+paru -S xidlehook ttf-mononoki brave-bin notification-daemon optimus-manager-qt xkb-switch apulse lxapperance mailspring mpv pacmanfm qt5ct ranger libpulse awesome git mlocate playerctl blueman bluez bluez-utils bluez-qt bluez-libs kdeconnect lm_sensors xtrlock transmission-gtk brightnessctl maim libreoffice-fresh bc 
 
 mkdir ~/.scripts
 sudo cp bingwall.sh /usr/bin
@@ -11,3 +11,4 @@ sudo cp toggle.sh /usr/bin
 sudo cp vol.sh /usr/bin
 cp kill.sh ~/.scripts
 cp switch.sh ~/.scripts
+cp toggle.py ~/.scripts
