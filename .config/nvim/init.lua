f92cf6725a9b96100059445390ae07c4e1d9406a
Stefan <stefan.jeftic122@gmail.com>
2023-03-25 16:32:57 +0100

    Change nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,34 +1,40 @@
-vim.defer_fn(function()
-  pcall(require, "impatient")
-end, 0)
+-- This is my personal Nvim configuration supporting Mac, Linux and Windows, with various plugins configured.
+-- This configuration evolves as I learn more about Nvim and become more proficient in using Nvim.
+-- Since it is very long (more than 1000 lines!), you should read it carefully and take only the settings that suit you.
+-- I would not recommend cloning this repo and replace your own config. Good configurations are personal,
+-- built over time with a lot of polish.
+--
+-- Author: Jiedong Hao
+-- Email: jdhao@hotmail.com
+-- Blog: https://jdhao.github.io/
+-- GitHub: https://github.com/jdhao
+-- StackOverflow: https://stackoverflow.com/users/6064933/jdhao
 
-require "core"
-require "core.options"
+local api = vim.api
+local utils = require("utils")
 
--- setup packer + plugins
-local fn = vim.fn
-local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
+-- check if we have the latest stable version of nvim
+local expected_ver = "0.8.3"
+local nvim_ver = utils.get_nvim_version()
 
-if fn.empty(fn.glob(install_path)) > 0 then
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
-  print "Cloning packer .."
-  fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
-
-  -- install plugins + compile their configs
-  vim.cmd "packadd packer.nvim"
-  require "plugins"
-  vim.cmd "PackerSync"
-
-  -- install binaries from mason.nvim & tsparsers
-  vim.api.nvim_create_autocmd("User", {
-    pattern = "PackerComplete",
-    callback = function()
-      vim.cmd "bw | silent! MasonInstallAll" -- close packer window
-      require("packer").loader "nvim-treesitter"
-    end,
-  })
+if nvim_ver ~= expected_ver then
+  local msg = string.format("Unsupported nvim version: expect %s, but got %s instead!", expected_ver, nvim_ver)
+  api.nvim_err_writeln(msg)
+  return
 end
 
-pcall(require, "custom")
+local core_conf_files = {
+  "globals.lua", -- some global settings
+  "options.vim", -- setting options in nvim
+  "autocommands.vim", -- various autocommands
+  "mappings.lua", -- all the user-defined mappings
+  "plugins.vim", -- all the plugins installed and their configurations
+  "colorschemes.lua", -- colorscheme settings
+}
 
-require("core.utils").load_mappings()
+-- source all the core config files
+for _, name in ipairs(core_conf_files) do
+  local path = string.format("%s/core/%s", vim.fn.stdpath("config"), name)
+  local source_cmd = "source " .. path
+  vim.cmd(source_cmd)
+end
