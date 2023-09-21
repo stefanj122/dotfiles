aa15f15bb9bf6da864b927034f6e7e5ef08c1bb7
stefanj122 <stefan.jeftic122@gmail.com>
2023-09-21 14:22:10 +0200

    Setting sam depedencies

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -17,7 +17,6 @@ require("telescope").setup({
 	},
 })
 
-require("telescope").load_extension("dap")
 require("telescope").load_extension("file_browser")
 require("telescope").load_extension("projects")
 
